86f37d69c2fc2aad4684089a956eb0a62372b18c
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-07-11 21:38:51 -0700

    Conditions

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+//
+//
+//
+//
+//
+//
+let cash = 30
+let price = 40
+let difference = cash - price
 
-
-
-
-
-
-let bool = '1' !== 1
-
-console.log(bool)
+if (cash > price) {
+    console.log(`you paid extra - the change is $${difference}`)
+} else if (cash === price) {
+    console.log("you paid the exact amount, have a nice day!")
+} else {
+    console.log(`you are $${-difference} short`)
+}
