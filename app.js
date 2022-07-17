67bfb8625755bae2a6c7f002cad1521d0f849b9a
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-07-17 14:37:24 -0700

    For loop

--- app.js
+++ app.js
@@ -4,9 +4,13 @@
 //
 //
 //
-let count = 1
+// let count = 1;
 
-while (count <= 100) {
-    console.log(count)
-    count = count + 1
+// while (count <= 100) {
+//   console.log(count);
+//   count = count + 1;
+// }
+
+for (let i = 0; i < 100; i++) {
+  console.log(i + 1)
 }
