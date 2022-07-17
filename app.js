6d1f49ee7fabe41f0f9314118dea6b2c26f2a041
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-07-17 15:24:32 -0700

    for loop exercise

--- app.js
+++ app.js
@@ -4,13 +4,16 @@
 //
 //
 //
-// let count = 1;
-
-// while (count <= 100) {
-//   console.log(count);
-//   count = count + 1;
-// }
-
-for (let i = 0; i < 100; i++) {
-  console.log(i + 1)
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 == 0) {
+    if (i % 5 == 0) {
+      console.log("Frontend Simplified");
+    } else {
+      console.log("Frontend");
+    }
+  } else if (i % 5 == 0) {
+    console.log("Simplified");
+  } else {
+    console.log(i);
+  }
 }
