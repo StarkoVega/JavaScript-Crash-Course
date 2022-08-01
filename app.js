9cee6780f9c6d835f086aab0f55b7e6e24d164b5
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-07-31 23:06:52 -0700

    Login example

--- app.js
+++ app.js
@@ -4,23 +4,44 @@
 //
 //
 //
-let users = [{
-  username: 'Jose',
-  email: 'jose.vega@gmail.com',
-  subscriptionStatus: 'VIP',
-  discordId: 'JoseVega#0001',
-  lessonsCompleted: [0,1]
-},
-{
-  username: 'Mitri',
-  email: 'mitri@gmail.com',
-  subscriptionStatus: 'VIP',
-  discordId: 'Mitri#0001',
-  lessonsCompleted: [0, 1, 2, 3]
-}]
+let users = [
+  {
+    username: "Jose",
+    email: "jose.vega@gmail.com",
+    password: "test123",
+    subscriptionStatus: "VIP",
+    discordId: "JoseVega#0001",
+    lessonsCompleted: [0, 1],
+  },
+  {
+    username: "Mitri",
+    email: "mitri@gmail.com",
+    password: "mitri123",
+    subscriptionStatus: "VIP",
+    discordId: "Mitri#0001",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
+  {
+    username: "Zen",
+    email: "zen@gmail.com",
+    password: "zen123",
+    subscriptionStatus: "VIP",
+    discordId: "Zen#0001",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
+];
 
-console.log(users[0].username)
+function login(email, password) {
+  for (let i = 0; i < users.length; ++i) {
+    if (users[i].email === email) {
+      console.log(users[i]);
+      if (users[i].password === password) {
+        console.log("log the user  in - the details are correct");
+      } else {
+        console.log("password is incorrect");
+      }
+    }
+  }
+}
 
-console.log(users[0].subscriptionStatus)
-
-console.log(users[0].lessonsCompleted.map(elem => elem * 2))
+login("jose.vega@gmail.com", "test123");
