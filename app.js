bc10395c7c3736e74ede1ff9bd6f4c6dd1e34ca1
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-07-31 22:53:09 -0700

    Objects Example

--- app.js
+++ app.js
@@ -4,8 +4,23 @@
 //
 //
 //
-let dollars = [1, 5, 10, 3]
+let users = [{
+  username: 'Jose',
+  email: 'jose.vega@gmail.com',
+  subscriptionStatus: 'VIP',
+  discordId: 'JoseVega#0001',
+  lessonsCompleted: [0,1]
+},
+{
+  username: 'Mitri',
+  email: 'mitri@gmail.com',
+  subscriptionStatus: 'VIP',
+  discordId: 'Mitri#0001',
+  lessonsCompleted: [0, 1, 2, 3]
+}]
 
-let cents = dollars.map((element) => element * 100)
+console.log(users[0].username)
 
-console.log(cents)
+console.log(users[0].subscriptionStatus)
+
+console.log(users[0].lessonsCompleted.map(elem => elem * 2))
